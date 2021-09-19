bc153bb4a4bcdebfe62d36878c73d7841157a8fc
sunil <msunilhyd@gmail.com>
2021-09-18 22:57:38 -0700

    18-Sep-21:Sat: Added the publishDate and curDate diff code

--- get_match_highlights.py
+++ get_match_highlights.py
@@ -1,6 +1,29 @@
+api_key = "AIzaSyBtfyFYMTFiCg6J9XgxZR4l0HVVrENPMMs"
+from apiclient.discovery import build
+youtube = build('youtube', 'v3', developerKey=api_key)
+# print(type(youtube))
+import urllib.request
+import json
+import datetime
+from datetime import date
+import maya
+
 with open('final_matches.txt', 'r') as f:
     data = f.readlines()
+    diff = 2
     for entry in  data:
         li = list(entry.split(','))
         league_name = li[0].replace('[', '')
         match = li[1]
+        req = youtube.search().list(q=match + ' highlights', part='snippet', type='video',
+                maxResults=10)
+        # print(type(req))
+        res = req.execute()
+        print(len(res['items']))
+        your_json = res['items']
+        publishedAt = your_json[0]['snippet']['publishedAt']
+        curDate = datetime.datetime.utcnow().isoformat() + "Z"
+        dt_1 = maya.parse(publishedAt).datetime()
+        dt_2 = maya.parse(curDate).datetime()
+
+        print(dt_2 - dt_1)
