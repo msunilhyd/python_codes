e96c934864e19e107d242231fe3406bb86a7b24b
linus <msunilhyd@gmail.com>
2022-01-06 19:34:40 -0600

    6-Jan-22: Generic commit

--- fixtures.py
+++ fixtures.py
@@ -15,17 +15,16 @@ with open('data.html', 'r') as f:
     for index, league in enumerate(leagues):
         print()
         league_name = league.find('h3', class_ = 'ContentPanel__ContentPanelTitle-sc-1izwmji-1 fyrfui ContentPanel__ContentPanelTitle-sc-1izwmji-1 fyrfui').text
-        if ('Champions League' in league_name):
-            list_of_league_objects = []
-            list_of_league_objects.append(league_name)
-            matches = league.find_all('div', class_ = 'Item__TeamScores-et8305-5 ehEtxo')
-            for match in matches:
-                teamA = match.find_all('span', class_ = 'Item__TeamA-et8305-6 jqhoIZ')
-                teamB = match.find_all('span', class_ = 'Item__TeamB-et8305-8 fFHtPs')
-                football_match = teamA[0].text + ' vs ' + teamB[0].text 
-                list_of_league_objects.append(football_match)
-            print(list_of_league_objects)
-            with open('final_matches.txt', 'w') as f:
-                print('heloo')
-                f.write(str(list_of_league_objects))
-                f.write('\n')
+        list_of_league_objects = []
+        list_of_league_objects.append(league_name)
+        matches = league.find_all('div', class_ = 'Item__TeamScores-et8305-5 ehEtxo')
+        for match in matches:
+            teamA = match.find_all('span', class_ = 'Item__TeamA-et8305-6 jqhoIZ')
+            teamB = match.find_all('span', class_ = 'Item__TeamB-et8305-8 fFHtPs')
+            football_match = teamA[0].text + ' vs ' + teamB[0].text 
+            list_of_league_objects.append(football_match)
+        print(list_of_league_objects)
+        with open('final_matches.txt', 'w') as f:
+            print('heloo')
+            f.write(str(list_of_league_objects))
+            f.write('\n')
